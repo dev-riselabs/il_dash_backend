6b6239c2de9dd6fd5730ca4ffb1df6d46409abb5
tsuccess <taofeeq.muhammad22@gmail.com>
2026-06-04 18:20:28 +0100

    generate slug

--- app/Models/Sector.php
+++ app/Models/Sector.php
@@ -5,12 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Sector extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'slug', 'color'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->slug) && $model->name) {
+                $model->slug = Str::slug($model->name);
+            }
+        });
+
+        static::updating(function ($model) {
+            if (empty($model->slug) && $model->name) {
+                $model->slug = Str::slug($model->name);
+            }
+        });
+    }
 
     public function deals(): HasMany
     {
--- app/Models/Track.php
+++ app/Models/Track.php
@@ -6,12 +6,30 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Track extends Model
 {
     use HasFactory;
 
     protected $fillable = ['event_id', 'name', 'slug', 'color'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->slug) && $model->name) {
+                $model->slug = Str::slug($model->name);
+            }
+        });
+
+        static::updating(function ($model) {
+            if (empty($model->slug) && $model->name) {
+                $model->slug = Str::slug($model->name);
+            }
+        });
+    }
 
     public function event(): BelongsTo
     {
